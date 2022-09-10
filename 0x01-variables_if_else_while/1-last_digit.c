40d9a54d6a7cb76309fd3c4e4ce6202542ea4fc2
blakk-ebuka <killduke101@gmail.com>
2022-09-09 21:47:33 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,4 +18,5 @@ int main(void)
                 printf("and is greater than 5\n");
         else if (n % 10 == 0)
                 printf("and is 0\n");
-        else (n % 10 < 6 && n % 10 != 0)                           printf("and is less than 6 and not 0\n");                                                           return (0);                                   }
+        else
+	       	printf("and is less than 6 and not 0\n");                                                           return (0);                                   }
