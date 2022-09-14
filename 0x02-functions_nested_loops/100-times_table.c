830f5588ccf7e41e28d16515dba80fae64a2f5d5
blakk-ebuka <killduke101@gmail.com>
2022-09-13 20:17:59 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the timea table
  */
-void print_times_table(void)
+void print_times_table(int n)
 {
 	int i, j, k;
 
