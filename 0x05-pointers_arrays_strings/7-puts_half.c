c228cc11e2f51b45c6c856659fd3bb5fe969a80e
blakk-ebuka <killduke101@gmail.com>
2022-09-20 09:23:44 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,18 @@
  */
 void puts_half(char *str)
 {
-	int count = 0, i;
+	int m, i;
 
-	while (count >= 0)
+	for (m = 0; str[m] != '\0'; m++);
+	if (m % 2 == 0)
 	{
-		if (str[count] == '\0')
-			break;
-		count++;
+		for (i = m / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	}
-	if (count
+	else
+	{
+	for (i = ((m - 1) / 2) + 1; str[i] != '\0'; i++)
+	_putchar(str[i]);
+	}
+	_putchar('\n');
 }
