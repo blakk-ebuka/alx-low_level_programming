1b41bba9867cedcbbc4de7043ca16fee864580f0
blakk-ebuka <killduke101@gmail.com>
2022-09-21 02:15:36 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: string to append to
+ * @src: string to add
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int m;
+
+	m = 0;
+
+	while (src[m] = '\0' && m < n)
+	{
+		dest[m] = src[m];
+		m++;
+	}
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+	return (dest);
+}
