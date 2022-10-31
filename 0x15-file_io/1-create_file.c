138e6ca2a705124bf38dbba70edf62c0bc2b729d
blakk-ebuka <killduke101@gmail.com>
2022-10-31 11:57:10 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  * @text_content: a pointer to a string to write to the file
  *
  * Return: if the function fails - -1.
- * otherwise - 1.
+ * otherwise -1.
  */
 int create_file(const char *filename, char *text_content)
 {
